cfa6879b2cd5bba005f8f157cd08afa70b9052bd
Benjamín <benja.ortega@gmail.com>
2019-01-20 18:07:54 -0300

    ADD: Index Home & Navbar

---
+++ assets/js/functions.js
@@ -0,0 +1,9 @@
+/* trigger when page is ready */
+$(function(){
+
+	$('.main_nav__hamb-link').on('click', function(event) {
+		event.preventDefault();
+		$('.main_nav__elements').toggleClass('show');
+	});
+
+})
