cd2e15bfb6767e4f800b2236fdc15dd223d7a0f9
Benjamín <benja.ortega@gmail.com>
2019-01-20 19:56:54 -0300

    ADD: Sección Inicio Final

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,5 +5,28 @@ $(function(){
 		event.preventDefault();
 		$('.main_nav__elements').toggleClass('show');
 	});
-
+	$('#main_skills__bar-html').LineProgressbar({
+	percentage: 85,
+	fillBackgroundColor: '#F11574'
+	});
+	$('#main_skills__bar-css').LineProgressbar({
+	percentage: 75,
+	fillBackgroundColor: '#F11574'
+	});
+	$('#main_skills__bar-js').LineProgressbar({
+	percentage: 60,
+	fillBackgroundColor: '#F11574'
+	});
+	$('#main_skills__bar-ps').LineProgressbar({
+	percentage: 75,
+	fillBackgroundColor: '#F11574'
+	});
+	$('#main_skills__bar-ai').LineProgressbar({
+	percentage: 85,
+	fillBackgroundColor: '#F11574'
+	});
+	$('#main_skills__bar-ae').LineProgressbar({
+	percentage: 60,
+	fillBackgroundColor: '#F11574'
+	});
 })
