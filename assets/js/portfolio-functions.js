b9dacfff52e410ad23268c23ad6c98b2206b713f
Benjamín <benja.ortega@gmail.com>
2019-01-20 23:58:02 -0300

    ADD: Portafolio - Home

---
+++ assets/js/portfolio-functions.js
@@ -0,0 +1,17 @@
+/* trigger when page is ready */
+$(function(){
+	$('.main_nav__hamb-link').on('click', function(event) {
+		event.preventDefault();
+		$('.main_nav__elements').toggleClass('show');
+	});
+	$('.portfolio_home__counter-first').CountUpCircle({
+		duration: 2000,
+		opacity_anim: false,
+		step_divider: 1
+	});
+  $('.portfolio_home__counter-second').CountUpCircle({
+    duration: 2000,
+    opacity_anim: false,
+    step_divider: 1
+  });
+})
