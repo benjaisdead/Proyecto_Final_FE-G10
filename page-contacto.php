8088b76e7c157048f54e517054c744bc38ac2d4e
Benjamín <benja.ortega@gmail.com>
2019-01-29 02:41:30 -0300

    ADD: Formulario de contacto

--- page-contacto.php
+++ page-contacto.php
@@ -20,27 +20,9 @@
 </div>
     <div class="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-12 col-xs-12">
       <div class="contact_form__inputs">
-        <form>
-          <div class="form-group">
-            <label for="exampleFormControlInput1">Nombre:</label>
-            <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Ingrese su nombre">
-          </div>
-            <div class="form-group">
-              <label for="exampleFormControlInput2">Email:</label>
-              <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="nombre@mail.com">
-            </div>
-            <div class="form-group">
-              <label for="exampleFormControlInput2">Teléfono:</label>
-              <input type="phone" class="form-control" id="exampleFormControlInput3" placeholder="Ingrese su teléfono">
-            </div>
-            <div class="form-group">
-              <label for="exampleFormControlTextarea1">Texto:</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-             <button type="submit" class="main_btn contact_form__btn">Enviar</button>
-          </form>
-      </div>
-    </div>
+
+          <?php echo do_shortcode('[contact-form-7 id="31" title="Formulario de contacto 1"]'); ?>
+</div>
   </section>
 
 
